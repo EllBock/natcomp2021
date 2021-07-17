32a8134f5da61790274db24bb98881417c3b34c9
Adriano <63779624+Adriano-Luongo@users.noreply.github.com>
2021-07-17 16:52:34 +0200

    Scheletro per multiprocessing della fitness

--- optimization.py
+++ optimization.py
@@ -1,12 +1,15 @@
 import pygmo as pg
 from pygmo import rosenbrock
-
+import matplotlib.pyplot as plt
 from optimizationProblem import optimizationProblem
 from pygmo.core import de1220, algorithm, population
 
-allowedVariants = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+
 
 if __name__ == "__main__":
+
+    allowedVariants = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                       18]  # Default [2, 3, 7, 10, 13, 14, 15, 16]
 
     # Initialize the problem
     problem = optimizationProblem()
@@ -14,8 +17,8 @@ if __name__ == "__main__":
     print(pgOptimizationProblem)
 
     # Initialize the algorithm
-    algo = algorithm(de1220(gen=300, variant_adptv=2, allowed_variants=allowedVariants))
-    algo.set_verbosity(200)
+    algo = algorithm(de1220(gen=200, variant_adptv=2, allowed_variants=allowedVariants))
+    algo.set_verbosity(1)
 
     # Run the algorithm
     pop = population(pgOptimizationProblem, 10**2)
@@ -23,6 +26,8 @@ if __name__ == "__main__":
 
     # Take the results
     print(f'Best parameters found are {pop.champion_x} and its score is {pop.champion_f}')
+    uda = algo.extract(de1220)
+    log = uda.get_log()
 
-
-
+    best_fitness = pop.get_f()[pop.best_idx()]
+    print(best_fitness)
--- optimizationProblem.py
+++ optimizationProblem.py
@@ -1,5 +1,84 @@
+import multiprocessing
+
 import pygmo as pg
 import numpy as np
+import json
+
+
+def writeXtoJson(x):
+
+
+    META_parameters = {
+        "METAclutchslip": 0,
+        "METAsortofontrack": -2,
+        "METAspincutslp": -1,
+        "METAsxappropriatest2": -2,
+        "METAsxappropriatest1": 0,
+        "METAstst": 0,
+        "METAs2cen": -2,
+        "METAsteer2edge": -3,
+        "METAbrake": -2,
+        "METAupsh5rpm": -1,
+        "METAupsh3rpm": -2,
+        "METAbackontracksx": -1,
+        "METAspincutclip": -1,
+        "METAoksyp": 0,
+        "METAupsh6rpm": 0,
+        "METAsycon2": -3,
+        "METAdnsh5rpm": -1,
+        "METAcarmin": -1,
+        "METAstC": 0,
+        "METAslipdec": -2,
+        "METAoffroad": -3,
+        "METAsycon1": 0,
+        "METAclutch_release": -3,
+        "METAbrakingpacefast": -1,
+        "METAclutchspin": -2,
+        "METAdnsh2rpm": -2,
+        "METAdnsh1rpm": -2,
+        "METAcarmaxvisib": 0,
+        "METAspincutint": 0,
+        "METAs2sen": -1,
+        "METAbrakingpaceslow": 0,
+        "METAsyint": -3,
+        "METAconsideredstr8": -2,
+        "METAsensang": -2,
+        "METAupsh2rpm": -2,
+        "METAst": -2,
+        "METAobvious": 0,
+        "METAsenslim": -3,
+        "METAupsh4rpm": -2,
+        "METAstr8thresh": 0,
+        "METAseriousABS": 0,
+        "METAskidsev1": -1,
+        "METAbackward": 0,
+        "METAfullstmaxsx": -2,
+        "METAsafeatanyspeed": 0,
+        "METAfullstis": -1,
+        "METAs2ang": -1,
+        "METApointingahead": 0,
+        "METAwheeldia": 0,
+        "METAsyslp": 0,
+        "METAwwlim": 0,
+        "METAignoreinfleA": 0,
+        "METAturnsetup": -1,
+        "METAdnsh4rpm": -1,
+        "METAaccinc": -2,
+        "METAdnsh3rpm": -2,
+        "METAobviousbase": -2,
+
+    }
+
+
+    parameters = json.dump(x)
+
+
+def executeGame(new_default_parameter):
+    pass
+
+
+def computefitness():
+    pass
 
 
 class optimizationProblem:
@@ -7,7 +86,35 @@ class optimizationProblem:
     # Deve ritornare un vettore (anche se il risultato è uno scalare) contenente la funzione di fitness valutata
     # nel punto x ( numpy array) .
     def fitness(self, x):
-        return [sum(x * x)]
+
+
+        new_default_parameter = writeXtoJson(x)
+
+        #3001
+        first = multiprocessing.Process(target=executeGame, args=('forza.xml',))
+
+        #3002
+        second = multiprocessing.Process(target=executeGame, args=('forza.xml',))
+
+        #3003
+        third = multiprocessing.Process(target=executeGame, args=('forza.xml',))
+
+        #3004
+        fourth = multiprocessing.Process(target=executeGame, args=('forza.xml',))
+
+        first.start()
+        second.start()
+        third.start()
+        fourth.start()
+
+        first.join()
+        second.join()
+        third.join()
+        fourth.join()
+
+        fitness = computefitness()
+
+        return fitness
 
     # Deve ritornare una coppia di np array contenente il minimo e massimo valore che i parametri possono assumere
     # I due array sono ordinati rispetto all'ordine dei parametri passati alla funzione di fitness.
