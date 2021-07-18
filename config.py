5d000e7312e897fb5b8499163fafc229c2c286d0
Adriano <63779624+Adriano-Luongo@users.noreply.github.com>
2021-07-18 17:46:58 +0200

    Config.py pubblico

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+PROJECTDIR = os.getcwd()
+TORCSDIR = r"D:\Games\torcs"
+#TORCSDIR = r"C:\Program Files (x86)\torcs"
+#TORCSDIR = r"C:\torcs"
+CLIENTPATH = os.path.abspath(os.path.join(PROJECTDIR, 'client.py'))
+
+FORZADIR = os.path.abspath(os.path.join(PROJECTDIR,'raceconfigs','forza'))
+ETRACKDIR = os.path.abspath(os.path.join(PROJECTDIR,'raceconfigs','etrack3'))
+CGTRACKDIR = os.path.abspath(os.path.join(PROJECTDIR,'raceconfigs','cgtrack2'))
+WHEELDIR = os.path.abspath(os.path.join(PROJECTDIR,'raceconfigs','wheel1'))
